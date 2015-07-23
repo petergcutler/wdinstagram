364e2c1a66c0ff86a6df1c9f214043ce3f13d7bf
mattscilipoti <matt.scilipoti@ga.co>
2015-07-22 23:48:31 -0400

    seed: remove sintra loader, removed ‘after’ “fix” due to error
    undefined method `after' for main:Object

--- db/connection.rb
+++ db/connection.rb
@@ -5,8 +5,3 @@ ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
   :database => "wdinstagram"
 )
-
-# Fix an issue with sinatra and Active Record where connections are left open
-after do
-  ActiveRecord::Base.connection.close
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 require 'bundler/setup'
-require "sinatra"
-require "sinatra/reloader"
 require "pry"
 
 require_relative "../db/connection"
